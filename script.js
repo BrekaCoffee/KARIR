39eb7e4e247f3ad6d5bd824564189809d68e4fb3
BREKA <121331251+BrekaCoffee@users.noreply.github.com>
2025-08-14 07:54:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,28 @@ window.addEventListener('load', () => {
     document.getElementById('loading-screen').style.display = 'none';
     document.getElementById('main-content').classList.remove('hidden');
   }, 3000);
+  var popup = document.getElementById("myPopup");
+    var img = document.getElementById("foto");
+    var popupImg = document.getElementById("popupImg");
+    var downloadLink = document.getElementById("downloadLink");
+    var closeBtn = document.getElementsByClassName("close")[0];
+
+    // Saat gambar diklik → tampilkan popup
+    img.onclick = function() {
+        popup.style.display = "block";
+        popupImg.src = this.src;
+        downloadLink.href = this.src; // set link download
+    }
+
+    // Saat klik tombol close
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // Klik di luar gambar untuk tutup
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
 });
