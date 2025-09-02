4aa74b7a1139ca8f02f49b87838a44cb894303c9
BREKA <121331251+BrekaCoffee@users.noreply.github.com>
2025-09-02 22:43:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-// 👀 Observer untuk efek fade-in saat scroll
-document.addEventListener('DOMContentLoaded', () => {
-  const fadeEl = document.querySelector('.fade-in');
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      }
-    });
-  }, { threshold: 0.2 });
-
-  observer.observe(fadeEl);
-});
-
-// ⏱️ Sembunyikan loading setelah 3 detik
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.getElementById('loading-screen').style.display = 'none';
-    document.getElementById('main-content').classList.remove('hidden');
-  }, 3000);
-  var popup = document.getElementById("myPopup");
-    var img = document.getElementById("foto");
-    var popupImg = document.getElementById("popupImg");
-    var downloadLink = document.getElementById("downloadLink");
-    var closeBtn = document.getElementsByClassName("close")[0];
-
-    // Saat gambar diklik → tampilkan popup
+// Saat gambar diklik → tampilkan popup
     img.onclick = function() {
         popup.style.display = "block";
         popupImg.src = this.src;
@@ -43,3 +17,4 @@ window.addEventListener('load', () => {
         }
     }
 });
+
